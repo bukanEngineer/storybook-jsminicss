532c93bf0b1fd97f5f8729bdb038f83147dbeb74
bukanEngineer <heispartofmyworks@gmail.com>
2023-12-01 14:16:31 +0700

    Fix error on js to jsx and error in card

---
+++ src/stories/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {options} from './constants'
+
+export const Card = ({
+    children = "im a card",
+    color = "primary" ,
+    size = "sm"
+})=> {
+    return <div>
+    {children}
+    </div>
+}
+
+Card.propTypes = {
+    children: PropTypes.node.isRequired,
+    color: PropTypes.oneOf(options.colors),
+    size: PropTypes.oneOf(options.size)
+}
+
+export default Card
---
+++ src/stories/Card/Card.stories.jsx
@@ -0,0 +1,23 @@
+import Card from "./Card";
+import { options } from "./constants";
+
+export default {
+    title: "Card",
+    component: Card,
+    tags: ['autodocs'],
+    argTypes: {
+      backgroundColor: { control: 'color' },
+    },
+}
+
+export const Default = () => <Card />;
+
+export const Colors = () =>
+  options.colors.map((color, index) => {
+    return <Card key={index} color={color} />;
+  });
+
+export const Sizes = () =>
+  options.sizes.map((size, index) => {
+    return <Card key={index} size={size} />;
+  });
